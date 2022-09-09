09c55750a99b46eda78382f4204d86e28dc00ef3
Mohammad Tahir <112794262+ghareebstar@users.noreply.github.com>
2022-09-09 10:37:57 +0500

    1st commit
    React Crypto Blog

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
+import RecentPosts from "./components/RecentPosts/RecentPosts";
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <RecentPosts />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
